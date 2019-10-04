ddedb3c8aab195cadeeaaf4902b48fb62c0993cd
samsonsugiyarto <samsonsugiyarto123@gmail.com>
2019-10-04 10:43:30 +0700

    perubahan database ke 2

--- sipnat.sql
+++ sipnat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 27 Sep 2019 pada 11.13
+-- Generation Time: 04 Okt 2019 pada 05.41
 -- Versi Server: 10.1.13-MariaDB
 -- PHP Version: 7.0.6
 
@@ -23,6 +23,113 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `nidn` int(15) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `jk` varchar(1) NOT NULL,
+  `mengajar` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `hp` int(30) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `role_id` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data untuk tabel `dosen`
+--
+
+INSERT INTO `dosen` (`nidn`, `name`, `password`, `jk`, `mengajar`, `email`, `hp`, `is_active`, `image`, `role_id`) VALUES
+(1997, 'oskar', '$2y$10$sIhxZ2Pe9CNyydtW8bscK.5NjkytG57cxWu7MnLWtkmTHmo8xcpKW', 'L', 'pemrogramman web', 'oskar@oskar.oskar', 9223, 1, 'default.jpg', 4),
+(9087, 'Dhany Faizal', '$2y$10$pU04d4t6uEWmGH6AeDojk.WzGdxIayk8LOJfpP1/57OBKkawR34su', 'L', 'Jaringan Komputer', 'dhany@gmail.com', 98654, 1, 'default.jpg', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jurusan`
+--
+
+CREATE TABLE `jurusan` (
+  `id` int(5) NOT NULL,
+  `nama_jurusan` varchar(50) NOT NULL,
+  `jumlah_mhs` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data untuk tabel `jurusan`
+--
+
+INSERT INTO `jurusan` (`id`, `nama_jurusan`, `jumlah_mhs`) VALUES
+(2, 'SI Teknik Multimedia dan Jaringan', 100),
+(3, 'SI Sistem Informasi', 210),
+(8, 'SI Teknik Informatika', 90),
+(9, 'D3 Komputerisasi Akuntansi', 70);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(15) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `jk` varchar(1) NOT NULL,
+  `jurusan` varchar(25) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `hp` int(15) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role_id` int(15) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `name`, `jk`, `jurusan`, `email`, `hp`, `image`, `password`, `role_id`, `is_active`) VALUES
+(2016, 'lala', 'P', 'S1 Teknik Multimedia & Ja', 'lala@lala.com', 812345, 'default.jpg', '$2y$10$2zGkETgfumnbf.UZg2gjlenXmduySJbYDlLKsmdINk/fQiUIrRlJa', 5, 1),
+(201601011, 'lili', 'P', 'SI Teknik Informatika', 'lili@lili.com', 91234, 'default.jpg', '$2y$10$XaJZKXdLWfZ2R.2xG5FShuuX2Im8I7/L5MUYgysnci2INB6aaKhcq', 5, 0),
+(201601030, 'Steven', 'L', 'SI Sistem Informasi', 'steven@steven.com', 812345, 'default.jpg', '$2y$10$DiTok9Xs/woXKoMlpplbGOBcTA2oE.8Cr..E5GBGIdLdkJ0f6aGkq', 5, 1),
+(201601050, 'Tini', 'P', 'D3 Komputerisasi Akuntans', 'tini@tini.com', 81234, 'default.jpg', '$2y$10$L6JjR1SMptVKIEZvrYPddugqun0SsYh1xOxxUMYWk7MwQTi/eCAi6', 5, 0),
+(201601060, 'Budi Santoso', 'L', 'S1 Sistem Informasi', 'budi@budi.budi', 812344, 'default.jpg', '$2y$10$8COs3lb7F/rMCnxn/inDMejVHiTsHnnkzceQJOkKJkxDnBvVq7pbG', 5, 1),
+(201601080, 'Doni', 'L', 'S1 Teknik Informatika', 'doni@doni.com', 81234, 'default.jpg', '$2y$10$OifSPuPKrh/Ffnd/OyUwsu.kYhHlPosOJ10FOga3M2dWBo3CN1qPm', 5, 1),
+(2016010111, 'Yulieus Adi Pranoto', 'L', 'SI', 'yuliusadi19@gmail.com', 2147483647, 'default.jpg', '$2y$10$lfL6OFZkI9BA6n2eUsZ7bu5xcw5xNgZzkFb5jl4VJW68LwF0myRTG', 5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pimpinan`
+--
+
+CREATE TABLE `pimpinan` (
+  `nidn` int(20) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `jk` varchar(1) NOT NULL,
+  `jabatan` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `hp` int(20) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `role_id` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pimpinan`
+--
+
+INSERT INTO `pimpinan` (`nidn`, `name`, `password`, `jk`, `jabatan`, `email`, `hp`, `is_active`, `image`, `role_id`) VALUES
+(1234, 'Romanus', '$2y$10$K0x3OlXyePafkAVwXml23.DeeRv3QYN6ij1hapvl3luBYVgj2j3pm', 'L', 'Ketua STIKOM', 'romanus@romanus.com', 812345, 1, 'default.jpg', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -35,7 +142,7 @@ CREATE TABLE `user` (
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
   `date_created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `user`
@@ -44,7 +151,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
 (3, 'Samson Sugiyarto', 'gitosamson123@gmail.com', 'DSC_2565a1.jpg', '$2y$10$y4bmK5JiJbIOYQ5ze0n4cei/QdNTnxIPcy8gsAc/9misiwt9W2keC', 1, 1, 1560654857),
 (4, 'Dila Fadillah', 'dila123@gmail.com', 'DSC_2565a.jpg', '$2y$10$dJfXzvLbBJU/aoJhY0oIlOC3rHvbSkvIow5i2I.zmR8hXX//YxaHm', 2, 1, 1560655500),
-(21, 'gito', 'samsonsugiyarto123@gmail.com', 'default.jpg', '$2y$10$hopfz/tuCVi5hV17nqOA5OejSIogOSjxwjDR4uyWFCM7SQx4P7RnG', 2, 1, 1561087895);
+(21, 'Samson Sugiyartoa', 'samsonsugiyarto123@gmail.com', 'default.jpg', '$2y$10$hopfz/tuCVi5hV17nqOA5OejSIogOSjxwjDR4uyWFCM7SQx4P7RnG', 2, 1, 1561087895);
 
 -- --------------------------------------------------------
 
@@ -56,7 +163,7 @@ CREATE TABLE `user_access_menu` (
   `id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   `menu_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `user_access_menu`
@@ -68,10 +175,22 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (13, 2, 2),
 (15, 1, 4),
 (16, 1, 6),
-(17, 1, 7),
 (18, 1, 3),
 (19, 1, 8),
-(20, 1, 9);
+(22, 1, 13),
+(23, 5, 2),
+(24, 5, 9),
+(25, 5, 10),
+(26, 4, 2),
+(27, 4, 9),
+(28, 4, 10),
+(29, 1, 7),
+(31, 2, 6),
+(32, 2, 13),
+(33, 3, 2),
+(34, 3, 9),
+(35, 3, 10),
+(39, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -82,7 +201,7 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 CREATE TABLE `user_menu` (
   `id` int(11) NOT NULL,
   `menu` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `user_menu`
@@ -94,7 +213,9 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 (3, 'Menu'),
 (6, 'Operation'),
 (7, 'Super Admin'),
-(9, 'Coblos');
+(9, 'Coblos'),
+(10, '(user) Komentar'),
+(13, 'Kandidat');
 
 -- --------------------------------------------------------
 
@@ -105,15 +226,18 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL,
   `role` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `user_role`
 --
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
-(1, 'Administrator'),
-(2, 'Member');
+(1, 'SuperAdmin'),
+(2, 'Admin'),
+(3, 'Pimpinan'),
+(4, 'Dosen'),
+(5, 'Mahasiswa');
 
 -- --------------------------------------------------------
 
@@ -128,7 +252,7 @@ CREATE TABLE `user_sub_menu` (
   `url` varchar(128) NOT NULL,
   `icon` varchar(128) NOT NULL,
   `is_active` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `user_sub_menu`
@@ -145,10 +269,12 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (9, 6, 'Jurusan', 'operation', 'fas fa-fw fa-graduation-cap', 1),
 (10, 6, 'Mahasiswa', 'operation/mahasiswa', 'fas fa-fw fa-child', 1),
 (11, 6, 'Dosen', 'operation/dosen', 'fas fa-fw fa-male', 1),
-(12, 6, 'Pimpinan ', 'operation/pimpinan', 'fas fa-fw fa-user-secret', 1),
+(12, 6, 'Pimpinan', 'operation/pimpinan', 'fas fa-fw fa-user-secret', 1),
 (13, 6, 'Kandidat', 'operation/kandidat', 'fas fa-fw fa-users', 1),
 (14, 6, 'Komentar', 'operation/komentar', 'fas fa-fw fa-comment', 1),
-(15, 9, 'Tentukan Pilihanmu!', 'coblos/pilih', 'fas fa-fw fa-check-square', 1);
+(15, 9, 'Tentukan Pilihanmu!', 'coblos', 'fas fa-fw fa-check-square', 1),
+(16, 10, 'Komentar', 'user/komentar', 'fas fa-fw fa-comment', 1),
+(17, 13, 'Riwayat', 'operation/riwayat', 'fas fa-fw fa-history', 1);
 
 -- --------------------------------------------------------
 
@@ -161,52 +287,81 @@ CREATE TABLE `user_token` (
   `email` varchar(128) NOT NULL,
   `token` varchar(128) NOT NULL,
   `date_created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`nidn`) USING BTREE;
+
+--
+-- Indexes for table `jurusan`
+--
+ALTER TABLE `jurusan`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`) USING BTREE;
+
+--
+-- Indexes for table `pimpinan`
+--
+ALTER TABLE `pimpinan`
+  ADD PRIMARY KEY (`nidn`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `user_token`
 --
 ALTER TABLE `user_token`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `jurusan`
+--
+ALTER TABLE `jurusan`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `user`
 --
@@ -216,22 +371,22 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 --
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `user_token`
 --
