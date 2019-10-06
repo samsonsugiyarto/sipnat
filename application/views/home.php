573cd0bd0afbb8927a6abd8437829b427ac5ff88
samsonsugiyarto <samsonsugiyarto123@gmail.com>
2019-10-06 18:30:01 +0700

    1. Penambahan di view (home) -> panel live voting, perbaikan panel komen 2. penambahan css -> riwayat dan perbaikan style (home)

--- application/views/home.php
+++ application/views/home.php
@@ -92,18 +92,61 @@
 
     <!-- Workingspace -->
     <div class="row workingspace">
-
         <div class="col-lg">
             <h2><span>Live Voting</span> </h2>
-            <p>Bekerja dengan suasana hati yang lebih asik, menyenangkan
-                dan mempelajari hal baru setiap harinya.</p>
+            <div class="row justify-content-around pt-4">
+                <div class="card-group">
+                    <div class="card cardfoto">
+                        <img src="<?= base_url('assets/img/profile/default.jpg') ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Axel Haryanto</h5>
+                            <p class="card-text">Ketua Senat</p>
+                        </div>
+                    </div>
+                    <div class="card cardfoto">
+                        <img src="<?= base_url('assets/img/profile/default.jpg') ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Hanit Jatmika</h5>
+                            <p class="card-text">Wakil Ketua Senat</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-group">
+                    <div class="card cardfoto">
+                        <img src="<?= base_url('assets/img/profile/default.jpg') ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Feni Lestari</h5>
+                            <p class="card-text">Ketua Senat.</p>
+                        </div>
+                    </div>
+                    <div class="card cardfoto">
+                        <img src="<?= base_url('assets/img/profile/default.jpg') ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Agnes Shita</h5>
+                            <p class="card-text">Wakil Ketua Senat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="row justify-content-around pt-3">
+        <h2><span>60%</span> </h2>
+        <h2><span>40%</span> </h2>
+    </div>
+    <div class="row justify-content-around">
+        <p>6 suara</p>
+        <p>4 Suara </p>
+    </div>
+    </div>
+
     </div>
     <!-- akhir Workingspace -->
 
 
     <!-- Gallery-->
-    <div class=" container2 gambar">
+
+    <div class=" container2 gambar ">
         <h1>Gallery Kegiatan SENAT</h1>
         <ul class="gambar2">
             <div class="center">
@@ -163,6 +206,7 @@
         </ul>
     </div>
 
+
     <!-- akhir Gallery -->
 
     <!-- TEAM -->
@@ -173,32 +217,32 @@
                     <h1>PERSONIL SENAT</h1>
                     Inilah personil SENAT periode 2017-2018
                 </div>
-                <div class="card">
+                <div class="cardedit">
                     <img src="assets/img/personil/feni.png" class="card-img-top" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Feni Lestari</h5>
-                        <p class="card-text">Ketua Senat</p>
+                    <div class="card-body card-body1 text-center">
+                        <h5 class="card-title card-title1">Feni Lestari</h5>
+                        <p class="card-text card-text1">Ketua Senat</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="cardedit">
                     <img src="assets/img/personil/shita.png" class="card-img-top" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Agnes Shita</h5>
-                        <p class="card-text"> Wakil Ketua Senat</p>
+                    <div class="card-body card-body1 text-center">
+                        <h5 class="card-title  card-title1">Agnes Shita</h5>
+                        <p class="card-text card-text1"> Wakil Ketua Senat</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="cardedit">
                     <img src="assets/img/personil/vebi.png" class="card-img-top" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Vebi</h5>
-                        <p class="card-text"> Sekretaris Senat</p>
+                    <div class="card-body card-body1 text-center">
+                        <h5 class="card-title  card-title1">Vebi</h5>
+                        <p class="card-text card-text1"> Sekretaris Senat</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="cardedit">
                     <img src="assets/img/personil/axel.png" class="card-img-top" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Axel Haryanto</h5>
-                        <p class="card-text"> Bendahara Senat</p>
+                    <div class="card-body card-body1 text-center">
+                        <h5 class="card-title  card-title1">Axel Haryanto</h5>
+                        <p class="card-text card-text1"> Bendahara Senat</p>
                     </div>
                 </div>
             </div>
@@ -206,56 +250,41 @@
     </div>
     </div>
     <!-- END TEAM -->
+    <div class="row justify-content-start">
+        <div class="col-12 komen">
+            <div class="row">
+                <div class="col-auto">
+                    <img style="max-width: 80px;" src="<?= base_url('assets/img/profile/default.jpg'); ?>" class="rounded-circle float-left" alt="...">
+                    <h6>Samson Sugiyarto</h6>
+                    <h5>Post on 12-10-2019 13:50</h5>
+                    <p>Hallo. jangan golput guys</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-auto komen2">
+                    <img style="max-width: 85px;" src="<?= base_url('assets/img/profile/default.jpg'); ?>" class="rounded-circle float-left" alt="...">
+                    <h6>Nabilla Nur Fadillah</h6>
+                    <h5>Post on 13-10-2019 12:53</h5>
+                    <p>Semoga pemilihan senat dilancarkan kegiatanya. amin</p>
+                </div>
+            </div>
 
-    <div class="row">
-        <div class="col-lg pl-5 pt-5">
-            <h4><b>2 komentar :</b></h4>
+
         </div>
+
     </div>
-    <div class=" pl-5 pt-3 mb-2">
-        <div class="row namakomen">
-            <img src="<?= base_url('assets/img/profile/default.jpg'); ?>" alt="foto-user" class="mr-3 mt-6 rounded-circle" style="width:70px;">
-            <b>Samson Sugiyarto</b><br>
-            <div class="row">
-                <div class="col-sm">
-                    <small> Posted on <i>2019-08-10 04:53:38</i></small>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-auto pl-5 ml-5">
-                <figure class="figurekomen komen">
-                    Ayo nyoblos, yang belum pada nyoblos, jangan golput ya
-                </figure>
-            </div>
-        </div>
-        <div class="mb-2">
-            <div class="row namakomen">
-                <img src="<?= base_url('assets/img/profile/default.jpg'); ?>" alt="foto-user" class="mr-3 mt-6 rounded-circle" style="width:70px;">
-                <b>Nabilla Nur Fadillah</b><br>
-                <div class="row">
-                    <div class="col-sm">
-                        <small> Posted on <i>2019-09-30 03:53:38</i></small>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-auto pl-5 ml-5">
-                    <figure class="figurekomen komen">
-                        hallo, semoga lancar dan sukses
-                    </figure>
-                </div>
-            </div>
-
-            <!-- akhir container -->
 
 
 
-            <!-- Optional JavaScript -->
-            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- akhir container -->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
