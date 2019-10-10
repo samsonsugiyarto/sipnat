d9064b0a492165dca10ae750bd239c7b9ce4d035
samsonsugiyarto <samsonsugiyarto123@gmail.com>
2019-10-11 01:41:53 +0700

    menambahkan fungsi urutan menu

--- sipnat.sql
+++ sipnat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 09 Okt 2019 pada 07.14
+-- Generation Time: 10 Okt 2019 pada 20.39
 -- Versi Server: 10.1.13-MariaDB
 -- PHP Version: 7.0.6
 
@@ -44,7 +44,7 @@ CREATE TABLE `dosen` (
 --
 
 INSERT INTO `dosen` (`nidn`, `name`, `password`, `jk`, `mengajar`, `email`, `hp`, `is_active`, `image`, `role_id`) VALUES
-(1997, 'oskar', '$2y$10$sIhxZ2Pe9CNyydtW8bscK.5NjkytG57cxWu7MnLWtkmTHmo8xcpKW', 'L', 'pemrogramman web', 'oskar@oskar.oskar', 9223, 1, 'default.jpg', 4),
+(1997, 'Endang', '$2y$10$sIhxZ2Pe9CNyydtW8bscK.5NjkytG57cxWu7MnLWtkmTHmo8xcpKW', 'L', 'pemrogramman web', 'oskar@oskar.oskar', 9223, 1, 'default.jpg', 4),
 (9087, 'Dhany Faizal', '$2y$10$pU04d4t6uEWmGH6AeDojk.WzGdxIayk8LOJfpP1/57OBKkawR34su', 'L', 'Jaringan Komputer', 'dhany@gmail.com', 98654, 1, 'default.jpg', 4);
 
 -- --------------------------------------------------------
@@ -66,7 +66,7 @@ CREATE TABLE `jurusan` (
 INSERT INTO `jurusan` (`id`, `nama_jurusan`, `jumlah_mhs`) VALUES
 (2, 'SI Teknik Multimedia dan Jaringan', 100),
 (3, 'SI Sistem Informasi', 210),
-(8, 'SI Teknik Informatika', 90),
+(8, 'SI Teknik Informatika', 95),
 (9, 'D3 Komputerisasi Akuntansi', 70);
 
 -- --------------------------------------------------------
@@ -93,7 +93,14 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`nim`, `name`, `jk`, `jurusan`, `email`, `hp`, `image`, `password`, `role_id`, `is_active`) VALUES
+(1, 'b', 'L', 'SI Sistem Informasi', 'b@b.b', 1, 'default3.jpg', '$2y$10$FQuPD.UOI0pHHAR8pyOoOeVuw.8japawncuk63IxIkNGQxKeVBghi', 5, 1),
+(2, 'a', 'L', 'SI Sistem Informasi', 'a@a.a', 1, 'DSC_2565a2.jpg', '$2y$10$h4aFHRlp3R4LPt60eRNIkeF9f2LSmOXG/g/EYI2oTsjZymG2qZt4W', 5, 1),
+(6, 's', 'L', 'SI Sistem Informasi', 's@s.s', 7, '1564848317473.jpg', '$2y$10$RdIH/z0BLEFS4gXZv/YJe.IDk039RCQrA51fP1jqj4rINX3t6ThOK', 5, 1),
+(8, '8', 'L', 'SI Sistem Informasi', 'o@o.o', 9, '1557978184889.jpg', '$2y$10$zIZ9msYrxFh/qMGdDmxdz.qPLIQAZbot7tBezkNUITpGMGvX9/ytq', 5, 1),
+(99, 'h', 'L', 'SI Sistem Informasi', 'h@h.h', 2, 'default.jpg', '$2y$10$99DGcD.IK1i1Zft6U6CCCezA4nrncCg6klKJS/ZrMq0vpXjMug9by', 5, 1),
 (2016, 'lala', 'P', 'S1 Teknik Multimedia & Ja', 'lala@lala.com', 812345, 'default.jpg', '$2y$10$2zGkETgfumnbf.UZg2gjlenXmduySJbYDlLKsmdINk/fQiUIrRlJa', 5, 1),
+(5454, 'g', 'L', 'SI Sistem Informasi', 'g@g.g', 1234, 'default.jpg', '$2y$10$K4v.ggu351RABgbJEFlf9.7SDApIvpGIpP4yotrkuzM4s/jEzdXU2', 5, 1),
+(5754, 'u', 'L', 'SI Sistem Informasi', 'u@u.u', 123, '1557978185058.jpg', '$2y$10$DePHJ5Fi.35UZch7vZkASuqdXTvaNiVqndHRYiEqU/f/3WqV2d1Tm', 5, 1),
 (201601011, 'lili', 'P', 'SI Teknik Informatika', 'lili@lili.com', 91234, 'default.jpg', '$2y$10$XaJZKXdLWfZ2R.2xG5FShuuX2Im8I7/L5MUYgysnci2INB6aaKhcq', 5, 0),
 (201601030, 'Steven', 'L', 'SI Sistem Informasi', 'steven@steven.com', 812345, 'default.jpg', '$2y$10$DiTok9Xs/woXKoMlpplbGOBcTA2oE.8Cr..E5GBGIdLdkJ0f6aGkq', 5, 1),
 (201601050, 'Tini', 'P', 'D3 Komputerisasi Akuntans', 'tini@tini.com', 81234, 'default.jpg', '$2y$10$L6JjR1SMptVKIEZvrYPddugqun0SsYh1xOxxUMYWk7MwQTi/eCAi6', 5, 0),
@@ -149,9 +156,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(3, 'Samson Sugiyarto', 'gitosamson123@gmail.com', 'default3.jpg', '$2y$10$y4bmK5JiJbIOYQ5ze0n4cei/QdNTnxIPcy8gsAc/9misiwt9W2keC', 1, 1, 1560654857),
+(3, 'Samson Sugiyarto', 'gitosamson123@gmail.com', 'default2.jpg', '$2y$10$y4bmK5JiJbIOYQ5ze0n4cei/QdNTnxIPcy8gsAc/9misiwt9W2keC', 1, 1, 1560654857),
 (4, 'Dila Fadillah', 'dila123@gmail.com', 'DSC_2565a.jpg', '$2y$10$dJfXzvLbBJU/aoJhY0oIlOC3rHvbSkvIow5i2I.zmR8hXX//YxaHm', 2, 1, 1560655500),
-(21, 'Samson Sugiyarto', 'samsonsugiyarto123@gmail.com', 'default.jpg', '$2y$10$hopfz/tuCVi5hV17nqOA5OejSIogOSjxwjDR4uyWFCM7SQx4P7RnG', 2, 1, 1561087895);
+(21, 'Samson Sugiyartoa', 'samsonsugiyarto123@gmail.com', 'default.jpg', '$2y$10$hopfz/tuCVi5hV17nqOA5OejSIogOSjxwjDR4uyWFCM7SQx4P7RnG', 2, 1, 1561087895);
 
 -- --------------------------------------------------------
 
@@ -190,8 +197,7 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (33, 3, 2),
 (34, 3, 9),
 (35, 3, 10),
-(39, 2, 1),
-(40, 1, 14);
+(39, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -201,23 +207,23 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 
 CREATE TABLE `user_menu` (
   `id` int(11) NOT NULL,
-  `menu` varchar(128) NOT NULL
+  `menu` varchar(128) NOT NULL,
+  `urutan_menu` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data untuk tabel `user_menu`
 --
 
-INSERT INTO `user_menu` (`id`, `menu`) VALUES
-(1, 'Admin'),
-(2, 'User'),
-(3, 'Menu'),
-(6, 'Operation'),
-(7, 'Super Admin'),
-(9, 'Coblos'),
-(10, '(user) Komentar'),
-(13, 'Kandidat'),
-(14, 'Administrator');
+INSERT INTO `user_menu` (`id`, `menu`, `urutan_menu`) VALUES
+(1, 'Admin', 2),
+(2, 'User', 4),
+(3, 'Menu', 6),
+(6, 'Operation', 5),
+(7, 'Super Admin', 1),
+(9, 'Coblos', 3),
+(10, '(user) Komentar', 7),
+(13, 'Riwayat Senat', 8);
 
 -- --------------------------------------------------------
 
@@ -276,8 +282,7 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (14, 6, 'Komentar', 'operation/komentar', 'fas fa-fw fa-comment', 1),
 (15, 9, 'Tentukan Pilihanmu!', 'coblos', 'fas fa-fw fa-check-square', 1),
 (16, 10, 'Komentar', 'user/komentar', 'fas fa-fw fa-comment', 1),
-(17, 13, 'Riwayat', 'operation/riwayat', 'fas fa-fw fa-history', 1),
-(18, 14, 'Admin', 'administrator', 'fas fa-fw fa-user', 1);
+(17, 13, 'Riwayat', 'operation/riwayat', 'fas fa-fw fa-history', 1);
 
 -- --------------------------------------------------------
 
@@ -374,12 +379,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 --
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `user_role`
 --
@@ -389,7 +394,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `user_token`
 --
